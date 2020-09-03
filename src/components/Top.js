68c493657b96761d1822d6c02036e0030e8da453
Adeshina Ibrahim <debowale01@gmail.com>
2020-09-03 23:00:45 +0100

    fixed the date

--- src/components/Top.js
+++ src/components/Top.js
@@ -2,12 +2,13 @@ import React from "react";
 
 const Top = (props) => {
   const { totalIncome, totalExpense, budgetValue, expensePercentage } = props;
+
   return (
     <div className="top">
       <div className="budget">
         <div className="budget__title">
           Available Budget in{" "}
-          <span className="budget__title--month">%Month%</span>:
+          <span className="budget__title--month">{getMonth()}</span>:
         </div>
 
         <div className="budget__value">
@@ -43,6 +44,51 @@ const Top = (props) => {
       </div>
     </div>
   );
+};
+
+const getMonth = () => {
+  const date = new Date();
+  let monthName;
+  const month = date.getMonth();
+
+  switch (month) {
+    case 0:
+      monthName = "January";
+      break;
+    case 1:
+      monthName = "February";
+      break;
+    case 2:
+      monthName = "March";
+      break;
+    case 3:
+      monthName = "April";
+      break;
+    case 4:
+      monthName = "May";
+      break;
+    case 5:
+      monthName = "June";
+      break;
+    case 6:
+      monthName = "July";
+      break;
+    case 7:
+      monthName = "August";
+      break;
+    case 8:
+      monthName = "September";
+      break;
+    case 9:
+      monthName = "October";
+      break;
+    case 10:
+      monthName = "November";
+      break;
+    default:
+      monthName = "December";
+  }
+  return monthName;
 };
 
 export default Top;
